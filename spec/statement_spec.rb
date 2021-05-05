051953c9e705d0288ecc3ea77d158b11e7d8999a
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-05-05 10:05:14 +0100

    refactored statement spec

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -20,7 +20,7 @@ describe Statement do
     context 'when a deposit is made' do
       it 'saves the deposit information to the account_history' do
         test_statement.save_deposit_history(deposit_one, test_date, test_balance)
-        expect(test_statement.account_history).to eq [{ Date: '10-01-2012', Deposit: 10, Withdraw: nil, Balance: 12 }]
+        expect(test_statement.account_history[0][:Deposit]).to eq 10
       end
     end
   end
@@ -29,7 +29,7 @@ describe Statement do
     context 'when a withdrawal is made' do
       it 'saves the withdrawal information to the account_history' do
         test_statement.save_withdraw_history(withdraw_one, test_date, test_balance)
-        expect(test_statement.account_history).to eq [{ Date: '10-01-2012', Deposit: nil, Withdraw: 5, Balance: 12 }]
+        expect(test_statement.account_history[0][:Withdraw]).to eq 5
       end
     end
   end
@@ -40,10 +40,10 @@ describe Statement do
       it 'retrieves the account history' do
         test_statement.save_deposit_history(deposit_one, test_date, test_balance)
         test_statement.save_withdraw_history(withdraw_one, test_date, test_balance)
-        expect(test_statement.retrieve_history).to eq [{ Date: '10-01-2012', Deposit: 10, Withdraw: nil, Balance: 12 },                                          { Date: '10-01-2012', Deposit: nil, Withdraw: 5, Balance: 12 }]
+        expect(test_statement.retrieve_history).to eq [{ Date: '10-01-2012', Deposit: 10, Withdraw: nil, Balance: 12 }, { Date: '10-01-2012', Deposit: nil, Withdraw: 5, Balance: 12 }]
       end
 
-      it 'includes a balance for each line on the statment' do
+      it 'includes a balance for each line on the statement' do
         test_statement.save_deposit_history(deposit_one, test_date, test_balance)
         test_statement.save_withdraw_history(withdraw_one, test_date, test_balance2)
         expect(test_statement.account_history[0][:Balance]).to eq 12
